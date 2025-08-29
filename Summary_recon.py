6db733cddaed1befbd1964dba2394478079b9d6d
Mahnoorasif-17 <mahnoor@ei1tech.com>
2025-08-29 15:08:25 +0500

    Update Summary_recon.py

--- Summary_recon.py
+++ Summary_recon.py
@@ -585,19 +585,5 @@ def run_reconciliation(batch_file, rta_file):
 
     return output_file
 
-st.title("Bank Reconciliation Tool")
-
-batch_file = st.file_uploader("Upload 1881 Batch File", type=["xlsx"])
-rta_file = st.file_uploader("Upload RTA File", type=["xlsx"])
-
-if st.button("Run Reconciliation"):
-    if batch_file and rta_file:
-        output_file = run_reconciliation(batch_file, rta_file)
-        st.success("Reconciliation complete!")
-
-        with open(output_file, "rb") as f:
-            st.download_button("Download Reconciliation Report", f, file_name=output_file)
-    else:
-        st.error("Please upload both files.")
 
 
